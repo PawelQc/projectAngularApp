80c6ad8a0165280c5488745adf3f735cebca5ed9
PawelQc <kucejko.pawel@gmail.com>
2020-06-09 15:28:14 +0200

    add ingredients of recipe to the view

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('A test Recipe1', 'Test desc1', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-    new Recipe('A test Recipe2', 'Test desc2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+    new Recipe(
+      'A test Recipe1',
+      'Test desc1',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'A test Recipe2',
+      'Test desc2',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+      [
+        new Ingredient('Bread', 1),
+        new Ingredient('Butter', 2)
+      ])
   ];
 
   getRecipes() {
